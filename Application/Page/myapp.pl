081b31aca84ca37f919d80ff491c69f6c43a626e
takuya.onda <takuya542@gmail.com>
2014-04-21 10:37:37 +0900

    fix

--- Application/Page/myapp.pl
+++ Application/Page/myapp.pl
@@ -15,54 +15,58 @@ use Logic::LocationData;
 use Logic::UserData;
 use Logic::CouponData;
 use Logic::Paging;
-use Plack::Session;
 
 # twitter認証
-my $config = Config::Const->get_twtter_key;
-my $nt = Net::Twitter::Lite::WithAPIv1_1->new(
+our $config = Config::Const->get_twtter_key;
+our $nt = Net::Twitter::Lite::WithAPIv1_1->new(
     consumer_key        => $config->{consumer_key},
     consumer_secret     => $config->{consumer_secret},
     access_token        => $config->{access_token},
     access_token_secret => $config->{access_token_secret},
-    ssl                 => 0,
+    ssl                 => 1,
 );
 
 get '/login' => sub {
     my $self = shift;
 
-    my $session      = Plack::Session->new( $self->req->env );
-    my $redirect_url = $self->param('redirect_url') || '/';
+
+    #$self->app->log->debug(Dumper($self->session));
+    #$self->app->log->debug( $self->req->url->base.'/callback' );
+    #$self->app->log->debug(Dumper($config));
+    #$self->app->log->debug(Dumper($nt));
 
     my $url     = $nt->get_authorization_url(
-        callback => $self->req->url->base . '/callback' 
+        callback => $self->req->url->base.'/callback'
     );
-    $session->set( 'token', $nt->request_token );
-    $session->set( 'token_secret', $nt->request_token_secret );
+    $self->session(expiration => (60 * 60 * 24 * 365 * 30));
+    $self->session( 'token'                 =>  $nt->request_token );
+    $self->session( 'token_secret'          =>  $nt->request_token_secret );
+    $self->session( 'redirect_after_login'  =>  '/' );
+    $self->redirect_to($url);
     $self->redirect_to('/');
 };
 
 
 get '/callback' => sub {
     my $self = shift;
-    my $session = Plack::Session->new( $self->req->env );
 
     unless ( $self->req->param('denied') ) {
 
         #request_token取得
-        $nt->request_token( $session->get('token') );
-        $nt->request_token_secret( $session->get('token_secret') );
+        $nt->request_token( $self->session('token') );
+        $nt->request_token_secret( $self->session('token_secret') );
 
         my $verifier = $self->req->param('oauth_verifier');
         my ( $access_token, $access_token_secret, $user_id, $screen_name ) =
             $nt->request_access_token( verifier => $verifier );
 
         # セッション発行
-        $session->set( 'access_token',        $access_token );
-        $session->set( 'access_token_secret', $access_token_secret );
-        $session->set( 'screen_name',         $screen_name );
+        $self->session( 'access_token'         => $access_token );
+        $self->session( 'access_token_secret'  => $access_token_secret );
+        $self->session( 'screen_name'          => $screen_name );
 
     }
-    $self->redirect_to( $session->get('redirect_url') );
+    $self->redirect_to( $self->session('redirect_after_login') );
 };
 
 
@@ -74,7 +78,6 @@ get '/:page' => { page => undef } => sub {
         request => $self->req, 
         param   => $self->param('page') || 1,
     });
-    $self->app->log->debug(Dumper($self->req));
     my $event_data = Logic::EventData->new($paging)->get_multi_event_data;
     $self->stash($event_data);
     ( $paging->is_sp ) ? $self->render('sp/index') : $self->render('pc/index')
@@ -103,7 +106,6 @@ get '/event/detail/:id' => sub{
     });
     my $event_data = Logic::EventData->new($paging)->get_single_event_data;
     $self->stash($event_data);
-    $self->app->log->debug("event_detail:".Dumper($self->stash));
     ( $paging->is_sp ) ? $self->render('sp/event_detail') : $self->render('pc/event_detail')
 };
 
